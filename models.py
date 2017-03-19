fix loss weighting

models.py
@@ -168,12 +168,10 @@ class Model(object):
     h.xhat = tf.cast(tf.argmax(h.exhat, axis=4), tf.uint8)
 
     h.losses = tfutil.softmax_xent(labels=h.px, logits=h.exhat)
-    # divide by number of variables masked out; this is the number of conditionals being trained
-    h.weights = 1. / tf.reduce_sum(1 - h.mask, axis=[1, 2, 3], keep_dims=True)
 
-    h.loss_total = tf.reduce_mean(h.losses * h.weights)
-    h.loss_given = tf.reduce_sum(h.losses * h.weights *      h.mask ) / tf.reduce_sum(    h.mask)
-    h.loss_asked = tf.reduce_sum(h.losses * h.weights * (1 - h.mask)) / tf.reduce_sum(1 - h.mask)
+    h.loss_total = tf.reduce_mean(h.losses)
+    h.loss_given = tf.reduce_sum(h.losses *      h.mask ) / tf.reduce_sum(    h.mask)
+    h.loss_asked = tf.reduce_sum(h.losses * (1 - h.mask)) / tf.reduce_sum(1 - h.mask)
 
     h.loss = h.loss_total if hp.optimize_given else h.loss_asked
 